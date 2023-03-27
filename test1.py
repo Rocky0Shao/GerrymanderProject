d41fbc34a3bdbbcbdffc288d2efb7fbf8f950df0
Rocky0Shao <122405175+Rocky0Shao@users.noreply.github.com>
2023-03-26 20:58:28 -0400

    add things

--- test1.py
+++ test1.py
@@ -4,7 +4,7 @@ import functions as f
 
 
 #this file should take in test1.jpg and return the contiguos, compactness of the blue districts.
-input_image = cv2.imread(r"C:\Users\labra\Gerry\test1.jpg")
+input_image = cv2.imread("test1.jpg")
 mask = f.maskForTest1(input_image)
 contours,_=cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
 for contour in contours:
